3ccc52f05b007bbb73ffb30f27988a8c63bb255c
Akash Manohar J <akash@akash.im>
2017-10-21 15:14:26 +0530

    Add ex1

--- ex1.c
+++ ex1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    puts("Hello World");
-    return 0;
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
 }
